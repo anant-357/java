e6aca13c87e9135d2808f829d6be38014cae1768
anant <95141712+anant-357@users.noreply.github.com>
2022-08-05 17:44:06 +0530

    BST formatted

--- ds/BST.java
+++ ds/BST.java
@@ -1,112 +1,101 @@
 package ds;
 
-
 public class BST {
 
-    public static class Node{
+    public static class Node {
         public int data;
         public Node left = null;
         public Node right = null;
 
-        public Node(int key){
+        public Node(int key) {
             this.data = key;
         }
 
-        public void addNode(int key){            
-            if(this.data>key){
-                if(right==null){
+        public void addNode(int key) {
+            if (this.data > key) {
+                if (right == null) {
                     right = new Node(key);
-                }            
-                else{
+                } else {
                     right.addNode(key);
                 }
-            }
-            else{
-                if(left==null){
+            } else {
+                if (left == null) {
                     left = new Node(key);
-                }            
-                else{
+                } else {
                     left.addNode(key);
-                }                
-            }            
+                }
+            }
         }
 
-        public boolean search(int key){
+        public boolean search(int key) {
 
-            if(data==key)
+            if (data == key)
                 return true;
 
-            if(data>key){
-                return right==null?false:right.search(key);
-            }
-            else{
-                return left==null?false:left.search(key);
+            if (data > key) {
+                return right == null ? false : right.search(key);
+            } else {
+                return left == null ? false : left.search(key);
             }
         }
-        
-        public void inorder(){
-            if(left!=null){
+
+        public void inorder() {
+            if (left != null) {
                 left.inorder();
             }
-            System.out.print(data+" ");
-            if(right!=null){
+            System.out.print(data + " ");
+            if (right != null) {
                 right.inorder();
             }
         }
 
-        public void preorder(){
-            System.out.print(data+" ");
-            if(left!=null){
+        public void preorder() {
+            System.out.print(data + " ");
+            if (left != null) {
                 left.preorder();
             }
-            if(right!=null){
+            if (right != null) {
                 right.preorder();
-            }            
+            }
         }
 
-        public void postorder(){
-            if(left!=null){
+        public void postorder() {
+            if (left != null) {
                 left.postorder();
-            }            
-            if(right!=null){
+            }
+            if (right != null) {
                 right.postorder();
             }
-            System.out.print(data+" ");
+            System.out.print(data + " ");
         }
-    
-        public Node minimumNode(){
-            if(left!=null){
+
+        public Node minimumNode() {
+            if (left != null) {
                 return left.minimumNode();
-            }
-            else{
+            } else {
                 return this;
             }
         }
-        
-        public Node deleteNode(int key){
 
-            if(data>key){
-                if(right!=null){
+        public Node deleteNode(int key) {
+
+            if (data > key) {
+                if (right != null) {
                     right = right.deleteNode(key);
-                }
-                else{
+                } else {
                     return this;
                 }
-            }
-            else if(data<key){
-                if(left!=null){
+            } else if (data < key) {
+                if (left != null) {
                     left = left.deleteNode(key);
-                }
-                else{
+                } else {
                     return this;
                 }
-            }
-            else{
-                if(left==null){
+            } else {
+                if (left == null) {
                     Node temp = right;
                     return temp;
-                }
-                else if(right ==null){
+                } else if (right == null) {
                     Node temp = left;
                     return temp;
                 }
@@ -122,7 +111,7 @@ public class BST {
         }
     }
 
-    public static void main(String... args){
+    public static void main(String... args) {
         Node root = new Node(20);
         root.addNode(25);
         root.addNode(15);
@@ -138,5 +127,5 @@ public class BST {
         System.out.println();
         System.out.println(root.search(10));
     }
-    
+
 }
